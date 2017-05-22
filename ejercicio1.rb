b9065814de430a8fe333a13c76c4991b9a6dd493
codeamac03 <codeamac03@gmail.com>
2017-05-22 10:23:23 -0500

    multiples impresiones

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -4,3 +4,6 @@ title = "Hola"
 description = "Bonito dia"
 number_of_likes = 30
 puts "title #{title} description #{description} number of likes #{number_of_likes}"
+puts "title #{title} description #{description} number of likes #{number_of_likes}"
+puts "title #{title} description #{description} number of likes #{number_of_likes}"
+puts "title #{title} description #{description} number of likes #{number_of_likes}"
